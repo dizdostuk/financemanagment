627b75b9e9ae0375b6fab4f2a166238f44fc3e70
Ulanbek Zhusupov <diz.dostuk@yandex.ru>
2019-11-01 00:24:55 +0300

    Styled cards

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
-import React, {Component} from 'react';
-import { Link, BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 import Navbar from './components/navbar/Navbar';
 import UserCard from './components/user-card/UserCard';
 import MainPage from './components/MainPage/MainPage';
 import Activities from './components/Activities/Activities';
 import Report from './components/Report/Report';
+import EditProfile from './components/EditProfile/EditProfile';
 
 function App() {
 
   return (
     <React.Fragment>
       <Router>
-      <Navbar />
-      <Switch>
-        <Route path="/">
-          <MainPage />
-        </Route>
-        <Route path="/activities">
-          <Activities />
-        </Route>
-        <Route path="/report">
-          <Report />
-        </Route>
-      </Switch>
-      <UserCard />
+        <Switch>
+          <Route path="/edit">
+            <EditProfile />
+          </Route>
+        </Switch>
+        <Navbar />
+        <Switch>
+          <Route path="/">
+            <MainPage />
+          </Route>
+          <Route path="/activities">
+            <Activities />
+          </Route>
+          <Route path="/report">
+            <Report />
+          </Route>
+        </Switch>
+        <UserCard />
       </Router>
     </React.Fragment>
   );
---
+++ src/components/EditProfile/EditProfile.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class EditProfile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (
+      <div className="edit-profile__wrapper">
+        Edit Profile
+      </div>
+    );
+  }
+}
+ 
+export default EditProfile;
