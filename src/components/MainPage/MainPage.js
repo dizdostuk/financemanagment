b71f270ffbdc98c70bd5947e7c33e5e5c3abbf34
Ulan <diz.dostuk@yandex.ru>
2019-11-01 22:28:37 +0300

    Added new operation html and gifted functionality

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -14,19 +14,25 @@ class MainPage extends Component {
           <div className="main-block-cards__wrapper">
             <section className="main-block__activities">
               <h3 className="main-block__activities__title">Поступления</h3>
-              <span className="main-block__activities__sum">+ 245 000 P</span>
+              <div className="main-block__activities__sum__wrapper">
+                <span className="main-block__activities__sum">+ 245 000 P</span>
+              </div>
             </section>
 
             <section className="main-block__activities">
               <h3 className="main-block__activities__title">Вычисления</h3>
-              <span className="main-block__activities__sum main-block__activities__sum--minus">- 5 000 P</span>
+              <div className="main-block__activities__sum__wrapper">
+                <span className="main-block__activities__sum main-block__activities__sum--minus">- 5 000 P</span>
+              </div>
             </section>
 
           </div>
 
           <section className="main-block__graph">
             <h3 className="main-block__graph__title">Изменения за этот год</h3>
-            
+            <div className="graph__interface">
+
+            </div>
           </section>
         </div>
       </div>
--- src/components/user-card/UserCard.js
+++ src/components/user-card/UserCard.js
@@ -7,16 +7,45 @@ import "./UserCard.css";
 class UserCard extends Component {
   constructor(props) {
     super(props);
-    this.state = {  };
+    this.state = {
+      balance: 345000,
+      firstName: "Улан",
+      lastName: "Жусупов",
+      worksAt: "Фронтенд разработчик Яндекс.Такси",
+      salary: 145000,
+      newOperation: 0,
+      changeValue: "income",
+    };
   }
+
+  handleoperation = (sum) => {
+    this.setState({newOperation: sum})
+  }
+
+  handleAddOperation = () => {
+    const { balance, newOperation, changeValue } = this.state;
+    if(changeValue === "income") {
+      this.setState({balance: parseInt(balance) + parseInt(newOperation)});
+    } else {
+      this.setState({balance: parseInt(balance) - parseInt(newOperation)})
+    }
+  }
+
+  handleOperationType = (opr) => {
+    this.setState({changeValue: opr});
+  }
+
   render() {
+    const { balance, firstName, lastName, worksAt, salary, changeValue } = this.state;
     return (
       <section className="user-card">
         <h2 className="visually-hidden">Карточка пользователя</h2>
         <div className="user-card__info card__info">
           <img className="card__info__img" src={JohnKheho} alt="User image"/>
-          <h4 className="card__info__name">Улан Жусупов</h4>
-          <p className="card__info__work-info">Фронтенд Разработчик Яндекс.Такси</p>
+          <h4 className="card__info__name">{firstName} {lastName}</h4>
+          <p className="card__info__work-info">{worksAt}</p>
+          <p className="card__info__work-info">${salary}</p>
+
         </div>
 
         <section className="user-finance">
@@ -24,7 +53,7 @@ class UserCard extends Component {
           <div className="user-finance__wrapper">
             <div className="user-finance__balance">
               <h5 className="user-finance__title">Баланс</h5>
-              <span className="balance">345 500Р</span>
+              <span className="balance">${balance}</span>
             </div>
             <div className="user-finance__balance">
               <h5 className="user-finance__title">Изменения</h5>
@@ -55,6 +84,22 @@ class UserCard extends Component {
               </a>
             </li>
           </ul>
+        </section>
+
+        <section className="new-transc">
+          <h3 className="new-transc__title">Новая операция</h3>
+          <div className="new-transc__form">
+            <p>
+              <select className="transc__list" value={changeValue} onChange={(e) => this.handleOperationType(e.value)}>
+                <option value="income">Поступления</option>
+                <option value="outcome">Вычисления</option>
+              </select>
+              <input onChange={(e) => this.handleoperation(e.target.value)} className="transc__inpt" type="number"/>
+            </p>
+            <p className="transc__btns">
+              <button className="transc__btn" onClick={() => this.handleAddOperation()}>Добавить</button>
+            </p>
+          </div>
         </section>
 
       </section>
